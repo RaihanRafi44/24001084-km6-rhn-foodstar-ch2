06188e9f8178a29e5292fb943aed086853c26cae
RaihanRafi44 <raihanrrizqullah@gmail.com>
2024-03-09 09:56:59 +0700

    Finishing Feature Home

---
+++ app/src/main/java/com/example/challengeandroidstudio/adapter/CategoryAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.challengeandroidstudio.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.challengeandroidstudio.databinding.ItemCategoryBinding
+import com.example.challengeandroidstudio.model.Category
+
+class CategoryAdapter : RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>(){
+
+    private val data = mutableListOf<Category>()
+
+    fun submitData(items: List<Category>){
+        data.addAll(items)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder {
+        return CategoryViewHolder(ItemCategoryBinding.inflate
+            (LayoutInflater.from(parent.context),
+            parent,
+            false))
+
+    }
+
+    //Counting the data size
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    class CategoryViewHolder(private val binding : ItemCategoryBinding) :
+        RecyclerView.ViewHolder(binding.root){
+        fun bind(item : Category){
+            binding.tvCategoryName.text = item.name
+            binding.ivCategoryImages.setImageResource(item.image)
+
+        }
+    }
+
+
+}
